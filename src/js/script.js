66f855abf71ba63df3aaf1738cb0ba4a7b46c1b1
krakenkro <sekimovab@gmail.com>
2020-12-15 17:11:29 +0600

    slider finish

--- src/js/script.js
+++ src/js/script.js
@@ -7,4 +7,15 @@ $(document).ready(function () {
     autoplay: true,
     autoplaySpeed: 1500,
   });
+  $(".offers__slider").slick({
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    centerMode: true,
+    speed: 2000,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    focusOnSelect: false,
+  });
 });
