fe923f886b59acab29b4d1e1c68a39261898bf1d
krakenkro <sekimovab@gmail.com>
2020-12-15 14:39:18 +0600

    reviews slider

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".reviews__slider").slick({
+    adaptiveHeight: false,
+    speed: 2000,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+  });
+});
